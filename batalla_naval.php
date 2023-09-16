c3d281a54ee1e09ae059b52d27a0b71c33c4d5a3
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2023-09-16 19:26:50 +0200

    batallas

--- batalla_naval.php
+++ batalla_naval.php
@@ -1,31 +1,88 @@
-<h2>Batalla Naval</h2>
-
 <style type="text/css">
 
 .estilo td {font-size:12px;border-width: 1px;padding: 8px;}
 
 </style>
 
-<table class="estilo" border="1" cellpadding="0" cellspacing="0" >
-
 <?php
 
-$cont=0;
-$M=10;
-$N=10;
-for ($i = 0; $i <= $M-1; $i++) {
+$filas = 10;
+$columnas = 10;
+$tablero = array_fill(0, $filas, array_fill(0, $columnas, 0));
 
-    echo "<tr>";
-        
-        for ($j = $cont; $j <= $N-1; $j++) {
+$inicio_fila = rand(0, $filas - 1);
+$inicio_columna = rand(0, $columnas - 1);
+$posicion_vertical = true; // Cambia a false para posición horizontal
+$longitud_submarino = 4; // Longitud del submarí
 
-            echo "<td>X</td>\n";
+if ($posicion_vertical) {
+    if ($inicio_fila + $longitud_submarino <= $filas) {
+        $puede_colocar = true;
+        for ($i = $inicio_fila; $i < $inicio_fila + $longitud_submarino; $i++) {
+            if ($tablero[$i][$inicio_columna] != 0) {
+                $puede_colocar = false;
+                break;
+            }
         }
-
-
-    echo "</tr>";
-
+    } else {
+        $puede_colocar = false;
+    }
+} else {
+    if ($inicio_columna + $longitud_submarino <= $columnas) {
+        $puede_colocar = true;
+        for ($j = $inicio_columna; $j < $inicio_columna + $longitud_submarino; $j++) {
+            if ($tablero[$inicio_fila][$j] != 0) {
+                $puede_colocar = false;
+                break;
+            }
+        }
+    } else {
+        $puede_colocar = false;
+    }
 }
-?>
 
-</table>
+if ($puede_colocar) {
+    if ($posicion_vertical) {
+        for ($i = $inicio_fila; $i < $inicio_fila + $longitud_submarino; $i++) {
+            $tablero[$i][$inicio_columna] = 1; // Representa el submarí como '1'
+        }
+    } else {
+        for ($j = $inicio_columna; $j < $inicio_columna + $longitud_submarino; $j++) {
+            $tablero[$inicio_fila][$j] = 1; // Representa el submarí como '1'
+        }
+    }
+}
+
+echo '<table class="estilo" border="1" cellpadding="0" cellspacing="0" >';
+
+for ($i = 0; $i <= $filas; $i++) {
+    echo "<tr scope='row' >";
+    if ($i == 0) {
+        for ($j = 0; $j <= $columnas; $j++) {
+            if ($j != 0) {
+                echo "<td>$j</td>\n";
+            } else {
+                echo "<td></td>\n";
+            }
+        }
+    } else {
+        if ($i <= $filas) {
+            for ($j = 0; $j <= $columnas; $j++) {
+                if ($j == 0) {
+                    echo "<td>" . chr(($i - 1) + 65) . "</td>";
+                } else {
+                    if ($tablero[$i - 1][$j - 1] == 1) {
+                        echo "<td>X</td>";
+                    } else {
+                        echo "<td></td>";
+                    }
+                }
+            }
+        }
+    }
+    echo "</tr>";
+}
+
+echo '</table>';
+
+?>
--- prueba.php
+++ prueba.php
@@ -1,87 +1,120 @@
+<style type="text/css">
+.estilo td {
+    font-size: 12px;
+    border-width: 1px;
+    padding: 8px;
+}
+</style>
+
 <?php
 
-// Funció per inicialitzar un taulell buit
-function inicialitzarTaulell($files, $columnes) {
-    $taulell = array_fill(0, $files, array_fill(0, $columnes, 0));
-    return $taulell;
-}
+$filas = 10;
+$columnas = 10;
+$tablero = array_fill(0, $filas, array_fill(0, $columnas, 0));
 
-// Funció per mostrar el taulell
-function mostrarTaulell($taulell) {
-    foreach ($taulell as $fila) {
-        echo implode(' ', $fila) . "\n";
-    }
-}
+// Define la flota con cantidades específicas de cada tipo de barco
+$flota = [
+    'fragatas' => 4,
+    'submarinos' => 3,
+    'destructores' => 2,
+    'portaaviones' => 1,
+];
 
-// Funció per col·locar un vaixell en el taulell
-function col·locarVaixell(&$taulell, $fila, $columna, $direccio, $mida) {
-    $files = count($taulell);
-    $columnes = count($taulell[0]);
+// Función para colocar un barco en el tablero de manera aleatoria
+function colocarBarco(&$tablero, $barco, $filas, $columnas)
+{
+    $longitud = $barco['longitud'];
+    $posicion_vertical = rand(0, 1) === 1; // Aleatoriamente vertical u horizontal
 
-    // Comprovar si es pot col·locar el vaixell
-    if (
-        ($direccio == 'horitzontal' && $columna + $mida > $columnes) ||
-        ($direccio == 'vertical' && $fila + $mida > $files)
-    ) {
-        return false; // No es pot col·locar el vaixell aquí
-    }
+    do {
+        $inicio_fila = rand(0, $filas - 1);
+        $inicio_columna = rand(0, $columnas - 1);
+    } while (!esPosicionValida($tablero, $inicio_fila, $inicio_columna, $longitud, $posicion_vertical));
 
-    // Comprovar si les cel·les estan buides
-    for ($i = 0; $i < $mida; $i++) {
-        if (
-            ($direccio == 'horitzontal' && $taulell[$fila][$columna + $i] != 0) ||
-            ($direccio == 'vertical' && $taulell[$fila + $i][$columna] != 0)
-        ) {
-            return false; // No es pot col·locar el vaixell aquí
+    if ($posicion_vertical) {
+        for ($i = $inicio_fila; $i < $inicio_fila + $longitud; $i++) {
+            $tablero[$i][$inicio_columna] = $barco['codigo'];
+        }
+    } else {
+        for ($j = $inicio_columna; $j < $inicio_columna + $longitud; $j++) {
+            $tablero[$inicio_fila][$j] = $barco['codigo'];
         }
     }
-
-    // Col·locar el vaixell
-    for ($i = 0; $i < $mida; $i++) {
-        if ($direccio == 'horitzontal') {
-            $taulell[$fila][$columna + $i] = 1;
-        } else {
-            $taulell[$fila + $i][$columna] = 1;
-        }
-    }
-
-    return true; // El vaixell s'ha col·locat amb èxit
 }
 
-// Funció per generar una partida aleatòria
-function generarPartidaAleatoria() {
-    $files = 10;
-    $columnes = 10;
-    $taulell = inicialitzarTaulell($files, $columnes);
+// Función para verificar si una posición es válida para colocar un barco
+function esPosicionValida($tablero, $fila, $columna, $longitud, $vertical)
+{
+    $filas = count($tablero);
+    $columnas = count($tablero[0]);
 
-    $vaixells = array(
-        'fragata' => 4,
-        'submarí' => 3,
-        'destructor' => 2,
-        'portaavions' => 1
-    );
+    if ($vertical) {
+        if ($fila + $longitud > $filas) {
+            return false;
+        }
 
-    foreach ($vaixells as $tipus => $quantitat) {
-        for ($i = 0; $i < $quantitat; $i++) {
-            $fila = rand(0, $files - 1);
-            $columna = rand(0, $columnes - 1);
-            $direccio = rand(0, 1) == 0 ? 'horitzontal' : 'vertical';
+        for ($i = $fila; $i < $fila + $longitud; $i++) {
+            if ($tablero[$i][$columna] !== 0) {
+                return false;
+            }
+        }
+    } else {
+        if ($columna + $longitud > $columnas) {
+            return false;
+        }
 
-            // Intentar col·locar el vaixell fins que es col·loqui correctament
-            while (!col·locarVaixell($taulell, $fila, $columna, $direccio, strlen($tipus))) {
-                $fila = rand(0, $files - 1);
-                $columna = rand(0, $columnes - 1);
-                $direccio = rand(0, 1) == 0 ? 'horitzontal' : 'vertical';
+        for ($j = $columna; $j < $columna + $longitud; $j++) {
+            if ($tablero[$fila][$j] !== 0) {
+                return false;
             }
         }
     }
 
-    return $taulell;
+    return true;
 }
 
-// Generar una partida aleatòria i mostrar el taulell
-$partida = generarPartidaAleatoria();
-mostrarTaulell($partida);
+// Coloca todos los barcos en el tablero
+foreach ($flota as $tipo => $cantidad) {
+    for ($i = 0; $i < $cantidad; $i++) {
+        $barco = [
+            'codigo' => strtoupper(substr($tipo, 0, 1)),
+            'longitud' => strlen($tipo),
+        ];
+        colocarBarco($tablero, $barco, $filas, $columnas);
+    }
+}
+
+// Muestra el tablero
+echo '<table class="estilo" border="1" cellpadding="0" cellspacing="0" >';
+
+for ($i = 0; $i <= $filas; $i++) {
+    echo "<tr scope='row' >";
+    if ($i == 0) {
+        for ($j = 0; $j <= $columnas; $j++) {
+            if ($j != 0) {
+                echo "<td>$j</td>\n";
+            } else {
+                echo "<td></td>\n";
+            }
+        }
+    } else {
+        if ($i <= $filas) {
+            for ($j = 0; $j <= $columnas; $j++) {
+                if ($j == 0) {
+                    echo "<td>" . chr(($i - 1) + 65) . "</td>";
+                } else {
+                    if ($tablero[$i - 1][$j - 1] == 1) {
+                        echo "<td>X</td>";
+                    } else {
+                        echo "<td>{$tablero[$i - 1][$j - 1]}</td>";
+                    }
+                }
+            }
+        }
+    }
+    echo "</tr>";
+}
+
+echo '</table>';
 
 ?>
-
