e40f944e50b40a0d50a48bf6220c8999a70654e4
MehdiRegg777 <mregbaoui.cf@iesesteveterradas.cat>
2023-11-17 12:17:07 +0100

    a

--- ejercicio4.php
+++ ejercicio4.php
@@ -10,48 +10,42 @@
 
 <?php
 
-$cont=0;
-$M=7;
-$N=7;
-for ($i = 0; $i <= $M; $i++) {
+$cont = 0;
+$M = 7;
+$N = 7;
 
-    echo "<tr scope='row' >";
-        if ($j == 0) {
-            
+echo "<table class='estilo' border='1' cellpadding='0' cellspacing='0'>";
+
+for ($i = 0; $i <= $M; $i++) {
+    echo "<tr scope='row'>";
+
+    if ($i == 0) {
+        for ($j = $cont; $j <= $N; $j++) {
+            if ($j != 0) {
+                echo "<td>$j</td>\n";
+            } else {
+                echo "<td></td>\n";
+            }
+        }
+    } else {
+        if ($i < 5) {
             for ($j = $cont; $j <= $N; $j++) {
-                if ($j != 0) {
-                    echo "<td>$j</td>\n";
-                }
-                else{
-                    echo "<td></td>\n";
+                if ($j == 0) {
+                    echo "<td>" . chr(($i - 1) + 65) . "</td>";
+                } else {
+                    echo "<td></td>";
                 }
             }
-
-        }else{
-            if ($i<5){
-
-                for ($j = $cont; $j <= $N; $j++) {
-                    if ($j==0){
-
-                        for ($j = 0; $j < $N + 1; $j++) {
-                            echo ($j == 0 ? "<td>".chr(($i-1)+65)."</td>" : "<td></td>");
-                        }
-
-
-                    } else {
-
-                        echo "<td></td>\n";
-                    }
-                }
-                
-            } 
         }
-       
+    }
 
     echo "</tr>";
-    
 }
+
+echo "</table>";
+
 ?>
+
 
 </table>
 
